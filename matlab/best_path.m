c5cd1c564cc7829212cb5e99a358cd8930210786
Matthew Klein <ggallin08@gmail.com>
2016-10-27 09:05:38 -0400

    Only check obstacles in the current path.

--- matlab/best_path.m
+++ matlab/best_path.m
@@ -3,11 +3,11 @@
 % intersects with that point. Finally, it plots the obstacle and the path.
 
 %% Create 'num_obs' obstacles at random x,y locations in front of the robot.
-num_obs = 1;
+num_obs = 10;
 xmax = 10;
 ymax = 10;
-x_obs = xmax*rand(num_obs,1);
-y_obs = 2*ymax*rand(num_obs,1)-ymax;
+x_obs_tot = xmax*rand(num_obs,1);
+y_obs_tot = 2*ymax*rand(num_obs,1)-ymax;
 
 %% Define vehicle (max width and speed)
 % Max width of vehicle
@@ -17,6 +17,18 @@ half_width = max_width/2;
 v_pre = 2;
 omega_pre = .1;
 R_pre = v_pre/omega_pre;
+
+% Find which obstacles are in the path
+% TODO: Add distance filter as well (only worry about close obstacles)
+in_path = sqrt(x_obs_tot.^2+(R_pre-y_obs_tot).^2)>=(R_pre-half_width) & ...
+          sqrt(x_obs_tot.^2+(R_pre-y_obs_tot).^2)<=(R_pre+half_width);
+x_obs = x_obs_tot(in_path);
+y_obs = y_obs_tot(in_path);
+
+% TODO: Once new path is found, see if an obstacle is in the new path. If a
+% larger change is needed to avoid any new obstacles, see if turning the
+% other direction is better. Keep checking until there are no obstacles in
+% the new path.
 
 %% Calculate ideal omega for obstacle avoidance
 % Calculate the radius of curvature of a circle that would just touch those
@@ -56,7 +68,10 @@ hold on
 plot(x_left,y_left,'Color',[0 0 0])
 plot(x_right,y_right,'Color',[0 0 0])
 
-for i = 1:num_obs
+% Plot all the obstacles with red x's
+plot(x_obs_tot,y_obs_tot,'xr','MarkerSize',10,'LineWidth',2)
+
+for i = 1:length(x_obs)
     y_mid = 0:sign(R_left(i)+half_width-(-half_width))*0.1:2*R_left(i);
     y_left = -half_width:sign(R_left(i)+half_width-(-half_width))*0.1:2*R_left(i)+half_width;
     y_right = half_width:sign(R_left(i)+half_width-(-half_width))*0.1:2*R_left(i)-half_width;
@@ -65,12 +80,11 @@ for i = 1:num_obs
     x_left  = abs( sqrt( half_width + 2*R_left(i) - y_left  ) .* sqrt( half_width + y_left  ));
     x_right = abs( sqrt( half_width - 2*R_left(i) + y_right ) .* sqrt( half_width - y_right ));
     
-    plot(x_obs(i),y_obs(i),'xr','MarkerSize',10,'LineWidth',2)
     plot(x_mid,y_mid,'--','Color',cols(mod(i,7)+1,:))
     plot(x_left,y_left,'Color',cols(mod(i,7)+1,:))
     plot(x_right,y_right,'Color',cols(mod(i,7)+1,:))
 end
-for i = 1:num_obs
+for i = 1:length(x_obs)
     y_mid = 0:sign(R_right(i)+half_width-(-half_width))*0.1:2*R_right(i);
     y_left = -half_width:sign(R_right(i)+half_width-(-half_width))*0.1:2*R_right(i)+half_width;
     y_right = half_width:sign(R_right(i)+half_width-(-half_width))*0.1:2*R_right(i)-half_width;
@@ -79,7 +93,6 @@ for i = 1:num_obs
     x_left  = abs( sqrt( half_width + 2*R_right(i) - y_left  ) .* sqrt( half_width + y_left  ));
     x_right = abs( sqrt( half_width - 2*R_right(i) + y_right ) .* sqrt( half_width - y_right ));
     
-    plot(x_obs(i),y_obs(i),'xr','MarkerSize',10,'LineWidth',2)
     plot(x_mid,y_mid,'--','Color',cols(mod(i+num_obs,7)+1,:))
     plot(x_left,y_left,'Color',cols(mod(i+num_obs,7)+1,:))
     plot(x_right,y_right,'Color',cols(mod(i+num_obs,7)+1,:))
