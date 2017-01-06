f6c8283f5093cbe708b660bf6ba863fdf156f805
Matthew A Klein <gmattklein@gmail.com>
2017-01-06 12:40:27 -0500

    Forgot to add goalPose to omega.

--- matlab/simulateKinematics.m
+++ matlab/simulateKinematics.m
@@ -3,7 +3,7 @@ function [T,X] = simulateKinematics(X0,tf,goalPose, v,omega)
 % Differential equations that determine state
 dxdt =@(t,x) [v(x,goalPose)*cos(x(3)); ...   % dx/dt
               v(x,goalPose)*sin(x(3)); ...   % dy/dt
-              omega(x)];            % dtheta/dt
+              omega(x,goalPose)];            % dtheta/dt
 
 % Timing (in seconds)
 ti=0; tspan=[ti tf];
